14d2f6d7c81933f5b30e5df38f089ca5aa2fdb5e
Stanislav Mamulin <StanislavPM@ya.ru>
2021-07-19 06:50:39 +0700

    ESLint and Prettier was added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-native',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/prefer-default-export': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+  },
 };
