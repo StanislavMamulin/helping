a429ef368ed42d83efdc73226914c9c2c93776ef
Stanislav Mamulin <StanislavPM@ya.ru>
2021-07-21 07:45:17 +0700

    V-01 Added prop-types library to check the types of passed props. Added checking of passed props in the Loader component.

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { ActivityIndicator } from 'react-native';
 
+import PropTypes from 'prop-types';
+
 const Loader = ({ animating }) => (
   <ActivityIndicator animating={animating} color="#7db354" size="large" />
 );
+
+Loader.propTypes = {
+  animating: PropTypes.bool.isRequired,
+};
 
 export default Loader;
