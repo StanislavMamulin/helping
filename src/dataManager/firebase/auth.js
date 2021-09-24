736e316c38f4aca1d119d99d6a11143d7ef7512b
Stanislav Mamulin <StanislavPM@ya.ru>
2021-09-24 08:58:45 +0700

    F-07 Added functions for login, create user and logout.

--- src/dataManager/firebase/auth.js
+++ src/dataManager/firebase/auth.js
@@ -1,16 +1,47 @@
+/* eslint-disable no-console */
 import auth from '@react-native-firebase/auth';
 
 export const firebaseSignInAnonymously = async () => {
-  await auth()
-    .signInAnonymously()
-    .then(() => {
-      console.log('Anonymous signed in');
-    })
-    .catch(error => {
-      if (error.code === 'auth/operation-not-allowed') {
-        console.log('Enable anonymous in your firebase console.');
-      }
+  try {
+    await auth().signInAnonymously();
+  } catch (err) {
+    if (err.code === 'auth/operation-not-allowed') {
+      console.log('Enable anonymous in your firebase console.');
+    }
 
-      console.error(error);
-    });
+    console.error(err);
+  }
+};
+
+export const signInWithEmailAndPassword = async (email, password) => {
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const createUserWithEmailAndPassword = async (email, password) => {
+  try {
+    await auth.createUserWithEmailAndPassword(email, password);
+  } catch (err) {
+    if (err.code === 'auth/email-already-in-use') {
+      console.log('That email address is already in use!');
+    }
+
+    if (err.code === 'auth/invalid-email') {
+      console.log('That email address is invalid!');
+    }
+
+    console.error(err);
+  }
+};
+
+export const signOut = async () => {
+  try {
+    await auth.signOut();
+    console.log('User signed out!');
+  } catch (err) {
+    console.error(err);
+  }
 };
