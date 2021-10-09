ff5015da13a5d16bd7aa5129a3eb623192be47af
Stanislav Mamulin <StanislavPM@ya.ru>
2021-10-09 14:42:46 +0700

    V-12 The layout of the screen "History. Empty"

--- src/screens/HistoryScreen/HistoryScreen.js
+++ src/screens/HistoryScreen/HistoryScreen.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+
+import { EmptyHistory } from '../../components/EmptyHistory/EmptyHistory';
+import { HistoryList } from '../../components/HistoryList/HistoryList';
+
 import styles from './styles';
 
-const HistoryScreen = () => {
+export const HistoryScreen = () => {
+  const [helpingHistory, setHelpingHistory] = useState([]);
+
+  useEffect(() => {
+    // getHistory
+  }, []);
+
   return (
-    <View>
+    <View style={styles.container}>
+      {helpingHistory.length === 0 ? <EmptyHistory /> : <HistoryList />}
     </View>
   );
 };
-
-export default HistoryScreen;
